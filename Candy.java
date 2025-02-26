652f6b1d42cf27d831660e3e55e7cf259f08a957
HaoXK <haoxuank@nycstudents.net>
2025-02-26 12:33:31 -0500

    Update Candy.java

--- Candy.java
+++ Candy.java
@@ -1,1 +1,16 @@
-Candy.java
+public class Candy 
+{
+    private String flavor;
+    public Candy(String f) {
+        flavor = f;
+    }
+   
+    public String getFlavor() 
+    {
+    return null;
+    }
+    
+    public String toString() {
+        return flavor;
+    }
+}
