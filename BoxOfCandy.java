05b045ce3446d41047abfc97413c7cd0ee3afdfe
HaoXK <haoxuank@nycstudents.net>
2025-02-26 12:33:01 -0500

    Update BoxOfCandy.java

--- BoxOfCandy.java
+++ BoxOfCandy.java
@@ -1,1 +1,34 @@
+public class BoxOfCandy
+{
+    private Candy[][] box;
 
+    public BoxOfCandy(Candy[][] b)
+    {
+        box = b;
+    }
+   
+    public boolean moveCandyToFirstRow(int col) {
+        if (box[0][col] != null) return true;
+        for (int i = 1; i < box.length; i++) {
+            if (box[i][col] != null) {
+                box[0][col] = box[i][col];
+                box[i][col] = null;
+                return true;
+            }
+        }
+        return false;
+    }
+    public Candy removeNextByFlavor(String flavor) {
+
+    }
+    public String toString() {
+        String s = "";
+        for (int row = 0; row < box.length; row++) {
+            for (int col = 0; col < box[0].length; col++) {
+                s += box[row][col] + " ";
+            }
+            s += "/n";
+        }
+        return s;
+    }
+}
